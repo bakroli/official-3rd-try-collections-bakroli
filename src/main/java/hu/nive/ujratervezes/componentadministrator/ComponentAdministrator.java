c32f005285a9cd81ad51f75b15c687ac3250860f
bakroli <bak.roland@hotmail.com>
2022-02-24 15:30:16 +0100

    Finish

--- src/main/java/hu/nive/ujratervezes/componentadministrator/ComponentAdministrator.java
+++ src/main/java/hu/nive/ujratervezes/componentadministrator/ComponentAdministrator.java
@@ -1,5 +1,27 @@
 package hu.nive.ujratervezes.componentadministrator;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ComponentAdministrator {
-    
+
+    public List<String> collectCommonComponents(List<String> firstCircuitComponents, List<String> secondCircuitComponents) {
+        if (firstCircuitComponents == null || secondCircuitComponents == null) {
+            return new ArrayList<>();
+        }
+        Set<String> resultSet = new HashSet<>();
+        for (String firstCircuit : firstCircuitComponents) {
+            for (String secondCircuit : secondCircuitComponents) {
+                if (firstCircuit.equals(secondCircuit)) {
+                    resultSet.add(firstCircuit);
+                }
+            }
+        }
+        List<String> result = new ArrayList<>();
+        result.addAll(resultSet);
+        return result;
+    }
+
 }
---
+++ src/main/java/hu/nive/ujratervezes/componentadministrator/Main.java
@@ -0,0 +1,8 @@
+package hu.nive.ujratervezes.componentadministrator;
+
+public class Main {
+    public static void main(String[] args) {
+        ComponentAdministrator c = new ComponentAdministrator();
+        System.out.println(c.collectCommonComponents(null,null));
+    }
+}
